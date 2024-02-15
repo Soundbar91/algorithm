46c4223113733a933191ab72e93748acd3d16908
신관규 <soundbar91@gmail.com>
2024-02-15 16:55:47 +0900

    [BOJ] 후위 표기식

---
+++ BOJ/Java/src/Gold/II/BOJ_1918.java
@@ -0,0 +1,59 @@
+package Gold.II;
+
+import java.io.*;
+import java.util.Stack;
+
+public class BOJ_1918 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        String str = br.readLine();
+
+        for (char ch : str.toCharArray()){
+            if (ch == '('){
+                stack.push(ch);
+            }
+            else if (ch == ')'){
+                while(!stack.empty()){
+                    if (stack.peek() == '('){
+                        stack.pop();
+                        break;
+                    }
+                    sb.append(stack.pop());
+                }
+            }
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+                while (!stack.empty()){
+                    char pop = stack.peek();
+                    if (precedence(ch) <= precedence(pop)){
+                        sb.append(stack.pop());
+                    }
+                    else break;
+                }
+                stack.push(ch);
+            }
+            else {
+                sb.append(ch);
+            }
+        }
+        while(!stack.empty()){
+            sb.append(stack.pop());
+        }
+        System.out.print(sb);
+    }
+
+    public static int precedence(char ch){
+        if (ch == '(' || ch == ')'){
+            return 0;
+        }
+        else if(ch == '+' || ch == '-'){
+            return 1;
+        }
+        else if(ch == '*' || ch == '/'){
+            return 2;
+        }
+        else return -1;
+    }
+}
