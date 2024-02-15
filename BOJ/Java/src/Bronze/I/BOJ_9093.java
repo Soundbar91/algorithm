efe5bad4e9b3ff336a5f94897ece4ca6868d81bd
신관규 <soundbar91@gmail.com>
2024-02-15 16:52:48 +0900

    [BOJ] 단어 뒤집기

---
+++ BOJ/Java/src/Bronze/I/BOJ_9093.java
@@ -0,0 +1,27 @@
+package Bronze.I;
+
+import java.io.*;
+import java.util.Stack;
+public class BOJ_9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        while(n-- > 0){
+            Stack<Character> stack = new Stack<>();
+            String str = br.readLine() + "\n";
+
+            for (char ch : str.toCharArray()){
+                if (ch == ' ' || ch == '\n'){
+                    while (!stack.empty()){
+                        bw.write(stack.pop());
+                    }
+                    bw.write(ch);
+                }
+                else stack.push(ch);
+            }
+        }
+        bw.flush();
+    }
+}
