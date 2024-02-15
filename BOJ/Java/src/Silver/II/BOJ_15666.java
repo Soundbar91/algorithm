2a5f3a661550ae133c94e6304b2e982876f62896
신관규 <soundbar91@gmail.com>
2024-02-15 17:06:20 +0900

    [BOJ] N과 M (12)

---
+++ BOJ/Java/src/Silver/II/BOJ_15666.java
@@ -0,0 +1,57 @@
+package Silver.II;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15666 {
+    static int[] node;
+    static int[] num;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        node = new int[M];
+        num = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+
+        dfs(0, 0);
+
+        System.out.print(sb);
+    }
+
+    public static void dfs(int index, int depth){
+
+        if (depth == M){
+            for (int i : node){
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        else{
+            int before = 0;
+
+            for (int i = index; i < N; i++){
+                if (before != num[i]){
+                    node[depth] = num[i];
+                    before = num[i];
+                    dfs(i, depth + 1);
+                }
+            }
+        }
+    }
+}
