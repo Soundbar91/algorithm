cec85cd5d4332e302ff208d00c64260e93fb4a32
신관규 <soundbar91@gmail.com>
2024-02-15 17:12:46 +0900

    [BOJ] 수 이어 쓰기 1

---
+++ BOJ/Java/src/Silver/IV/BOJ_1748.java
@@ -0,0 +1,24 @@
+package Silver.IV;
+
+import java.io.*;
+
+public class BOJ_1748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        int cnt = 10;
+        int weight = 1;
+
+        for(int i = 1; i <= n; i++){
+            if (i % cnt == 0){
+                weight += 1;
+                cnt *= 10;
+            }
+            result += weight;
+        }
+
+        System.out.print(result);
+    }
+}
