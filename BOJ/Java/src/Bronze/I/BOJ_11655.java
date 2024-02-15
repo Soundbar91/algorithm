d37ee112bb2154309b1de3158a6d9d5c11b3c562
신관규 <soundbar91@gmail.com>
2024-02-15 16:52:55 +0900

    [BOJ] ROT13

---
+++ BOJ/Java/src/Bronze/I/BOJ_11655.java
@@ -0,0 +1,23 @@
+package Bronze.I;
+
+import java.io.*;
+public class BOJ_11655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        for (char ch : str.toCharArray()){
+            if (65 <= ch && 90 >= ch){
+                sb.append((char)(65 + (((int)(ch - 'A') + 13)) % 26));
+            }
+            else if (97 <= ch && 122 >= ch){
+                sb.append((char)(97 + (((int)(ch - 'a') + 13)) % 26));
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb);
+    }
+}
