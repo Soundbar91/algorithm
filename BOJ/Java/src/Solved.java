63091519b0bef92b376f2843998c647ae8213ee6
신관규 <soundbar91@gmail.com>
2024-02-15 16:50:21 +0900

    Create Solved.java

---
+++ BOJ/Java/src/Solved.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Solved {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+    }
+}
