3a0c270792b52694ad167e4d18a24de2982f0117
신관규 <soundbar91@gmail.com>
2024-02-15 17:08:54 +0900

    [BOJ] 소수 구하기

---
+++ BOJ/Java/src/Silver/III/BOJ_1929.java
@@ -0,0 +1,35 @@
+package Silver.III;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_1929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] numArr = new int[N + 1];
+
+        for (int i = 2; i <= N; i++){
+            numArr[i] = i;
+        }
+        for (int i = 2; i <= Math.sqrt(N); i++){
+            if (numArr[i] == 0) {
+                continue;
+            }
+
+            for (int j = i + i; j <= N; j += i){
+                numArr[j] = 0;
+            }
+        }
+
+        for (int i = M; i <= N; i++){
+            if (numArr[i] != 0) {
+                System.out.println(numArr[i]);
+            }
+        }
+    }
+}
