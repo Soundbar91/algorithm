a615dd4707bdd53f6abab744d7b9427a7f8384bc
신관규 <soundbar91@gmail.com>
2024-02-15 17:13:44 +0900

    [BOJ] 접미사 배열

---
+++ BOJ/Java/src/Silver/IV/BOJ_11656.java
@@ -0,0 +1,26 @@
+package Silver.IV;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ_11656 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        String[] chArr = new String[str.length()];
+
+        for (int i = 0; i < str.length(); i++){
+            chArr[i] = str.substring(i);
+        }
+        Arrays.sort(chArr);
+
+        for(int i = 0; i < chArr.length; i++){
+            bw.write(chArr[i]);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
