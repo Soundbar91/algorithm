b858effa43bd45188e718076a5c4032a5f24fc5d
신관규 <soundbar91@gmail.com>
2024-03-03 15:40:50 +0900

    [BOJ] 카트 놓기

---
+++ BOJ/Java/src/Silver/III/BOJ_18115.java
@@ -0,0 +1,35 @@
+package Silver.III;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_18115 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(new StringBuilder(br.readLine())
+                .reverse().toString());
+        for (int i = 1; i <= N; i++) {
+            int listValue = Integer.parseInt(st.nextToken());
+
+            if (listValue == 1) deque.addFirst(i);
+            else if (listValue == 2) {
+                int tmp = deque.removeFirst();
+                deque.addFirst(i);
+                deque.addFirst(tmp);
+            }
+            else {
+                deque.addLast(i);
+            }
+        }
+
+        while (!deque.isEmpty()) {
+            bw.write(Integer.toString(deque.poll()) + " ");
+        }
+        bw.flush();
+    }
+}
