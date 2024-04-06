df7dc34d4341e88891eb0ee9788533b5c418f311
신관규 <soundbar91@gmail.com>
2024-04-06 10:04:43 +0900

    [BOJ] 탑

---
+++ BOJ/Java/src/Gold/V/BOJ_2493.java
@@ -0,0 +1,31 @@
+package Gold.V;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_2493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<int[]> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            int top = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty()) {
+                if (stack.peek()[1] >= top) {
+                    System.out.print(stack.peek()[0] + " ");
+                    break;
+                }
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                System.out.print("0 ");
+            }
+            stack.push(new int[]{i, top});
+        }
+    }
+}
