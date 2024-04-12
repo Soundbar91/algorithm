52cdc3f61194c48f43c74400acba2db2fec49025
신관규 <soundbar91@gmail.com>
2024-04-12 09:23:45 +0900

    [BOJ] 문제집

---
+++ BOJ/Java/src/Gold/II/BOJ_1766.java
@@ -0,0 +1,53 @@
+package Gold.II;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_1766 {
+    static int N;
+    static int M;
+    static int[] edgeCount;
+    static ArrayList<ArrayList<Integer>> map = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        edgeCount = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) map.add(new ArrayList<>());
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            map.get(x).add(y);
+            edgeCount[y]++;
+        }
+
+        solve();
+    }
+
+    public static void solve() {
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int i = 1; i <= N; i++) if (edgeCount[i] == 0) queue.add(i);
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            System.out.print(cur + " ");
+
+            for (int i : map.get(cur)) {
+                edgeCount[i]--;
+                if (edgeCount[i] == 0) queue.add(i);
+            }
+        }
+    }
+}
