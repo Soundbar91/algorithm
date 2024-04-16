9d4455109f4ce9ac506d414b7932b2f3b081bd04
신관규 <soundbar91@gmail.com>
2024-04-16 16:37:23 +0900

    [BOJ] 부분수열의 합

--- BOJ/Java/src/Silver/II/BOJ_1182.java
+++ BOJ/Java/src/Silver/II/BOJ_1182.java
@@ -6,26 +6,31 @@ import java.util.StringTokenizer;
 public class BOJ_1182 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
         int[] numArr = new int[N];
-        int result = 0;
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++){
-            numArr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) numArr[i] = Integer.parseInt(st.nextToken());
+
+        int result = solve(0, 0, N, S, numArr);
+
+        if (S == 0) result--;
+        System.out.println(result);
+    }
+
+    public static int solve(int depth, int sum, int N, int S, int[] A) {
+        if (depth == N) {
+            if (sum == S) return 1;
+            else return 0;
         }
 
-        for (int i = 1; i < (1 << N); i++){ /* 부분 순열 */
-            int sum = 0;
-            for (int j = 0; j < N; j++){ /* 부분 순열에 어느 원소가 포함되어 있는지 */
-                if ((i & (1 << j)) != 0) sum += numArr[j];
-            }
-            if (sum == S) result++;
-        }
+        int count = 0;
+        count += solve(depth + 1, sum + A[depth], N, S, A);
+        count += solve(depth + 1, sum, N, S, A);
 
-        System.out.print(result);
+        return count;
     }
 }
