13a1e627bada698c6adefcbe9ecfc0e7532cd1d8
신관규 <soundbar91@gmail.com>
2024-05-02 12:59:04 +0900

    [BOJ] 큰 수 만들기

---
+++ BOJ/Java/src/Platinum/V/BOJ_16496.java
@@ -0,0 +1,29 @@
+package Platinum.V;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_16496 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[] nums = Arrays.stream(br.readLine().split(" "))
+                .toArray(String[]::new);
+
+        System.out.println(solve(nums));
+    }
+
+    public static String solve(String[] nums) {
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(nums, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return -(o1 + o2).compareTo(o2 + o1);
+            }
+        });
+
+        for (String i : nums) sb.append(i);
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
+    }
+}
