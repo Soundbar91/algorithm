143666592105ef5af9e78450feed5db91f44a88a
신관규 <soundbar91@gmail.com>
2024-05-23 13:10:29 +0900

    [BOJ] 징검다리 건너기 (small)

---
+++ BOJ/Java/src/Silver/I/BOJ_22869.java
@@ -0,0 +1,42 @@
+package Silver.I;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_22869 {
+    static int N;
+    static int K;
+    static int[] stone;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        stone = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) stone[i] = Integer.parseInt(st.nextToken());
+
+        System.out.print(solve() ? "YES" : "NO");
+        br.close();
+    }
+
+    public static boolean solve() {
+        boolean[] dp = new boolean[N];
+        dp[0] = true;
+
+        for (int i = 0; i < N; i++) {
+            if (dp[i]) {
+                for (int j = i + 1; j < N; j++) {
+                    if ((j - i) * (Math.abs(stone[i] - stone[j]) + 1) <= K) {
+                        dp[j] = true;
+                    }
+                }
+            }
+        }
+
+        return dp[N - 1];
+    }
+}
